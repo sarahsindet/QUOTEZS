81f84c5f7c080db683d668f83fb5a9ac66e81d1c
sarahsindet <sarahsindet@gmail.com>
2020-06-17 12:00:06 +0300

    added a contructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,8 @@
 export class Quote {
-}
+    constructor(
+        public text: string, public author: string, public submitter: string,
+        public upvotes: number,
+        public downvotes: number
+        ) { }
+    
+    }
