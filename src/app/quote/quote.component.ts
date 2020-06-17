24a0f48b2f058b69486c1eff319cb3b1d9ed048a
sarahsindet <sarahsindet@gmail.com>
2020-06-17 05:41:07 +0300

    added afew qutes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  
+
+  
+  delete(i) {
+    this.quotes.splice(i, 1)
+  }
+
+  addNewQuote(quote){
+    let quoteLength= this.quotes.length;
+    quote.text=quote.text;
+    quote.author=quote.author;
+    quote.submitter=quote.submitter;
+    quote.upvotes=quote.upvotes;
+    quote.downvotes=quote.downvotes
+
+
+    this.quotes.push(quote)
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
