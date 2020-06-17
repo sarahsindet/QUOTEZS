f9218c000a95d7b9f08a3038b21d202d0f54226e
sarahsindet <sarahsindet@gmail.com>
2020-06-17 11:11:20 +0300

    added two more quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,9 +10,9 @@ export class QuoteComponent implements OnInit {
   
 
   quotes: Quote[] = [
-    new Quote('Sarah','Daniel','The Most lethal weapon a human being can make is a team',new Date(20/12/2019)),
-    new Quote('Daniel','Daniel','Its one thing to go to school to learn is another Expirience is one thing growth is another',new Date(12/1/2000)),
-    new Quote('James','Barrack Obama','Yes We Can',new Date(13/12/1996)),
+    new Quote('Sarah','Do your best until your good is better and your better is best',new Date(20/8/2009)),
+    new Quote('livingstone','Courage is to beat every odds in life',new Date(6/1/2000)),
+    new Quote('Barrack Obama','Yes We Can',new Date(13/12/1996)),
   
   ];
 
